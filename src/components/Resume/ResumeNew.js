a123014af3010b075d95ae74892abc70971069d3
Bree Kagwe <kagwe.bridgette@gmail.com>
2026-09-23 13:42:22 +1000

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
+import WriteupPDF from "../Projects/Writeups/WriteupPDF";
 import pdf from "../../Assets/Placeholder_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
 
 // EDIT THIS PART:
 // The import above on line 5 where it says Placeholder_CV.pdf, change it to your resume file name.
@@ -38,9 +35,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          <WriteupPDF pdf={pdf} />
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
